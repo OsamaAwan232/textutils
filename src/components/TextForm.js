8a4ffcb1a75437d4a56540a9116dffaf67e76fa1
Osama Awan <osamaawan768@gmail.com>
2024-04-04 10:24:59 +0500

    Adding more logic to textutils

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,19 +7,38 @@ const TextForm = (props) => {
         setText(newText)
     }
 
+    const handleLoClick = ()=>{
+        let newText = text.toLowerCase()
+        setText(newText)
+    }
+
     const handleOnchange = (event)=>{
         setText(event.target.value)
     }
-    const [text, setText] = useState('Enter text here')
+    const [text, setText] = useState('')
     return (
-        <div>
+        <>
+        <div className='container'>
             <h1>{props.heading}</h1>
             <div className="mb-3">
                 <textarea className="form-control" value={text} onChange={handleOnchange} id="Box" rows="8"></textarea>
             </div>
-            <button className="btn btn-danger" onClick={handleUpClick}>ToUpperCase</button>
+            <button className="btn btn-danger mx-2" onClick={handleUpClick}>ToUpperCase</button>
+            <button className="btn btn-danger mx-2" onClick={handleLoClick}>ToLowerCase</button>
         </div>
+        <div className="container">
+            <h1>Your text summery</h1>
+            <p>{text.split(' ').length} words and {text.length} characters</p>
+            <p>{0.008 * text.slice(' ').length}Minutes read</p>
+            <h2>Preview</h2>
+            <p>{text}</p>
+        </div>
+        </>
+    
     )
 }
 
 export default TextForm
+
+
+
