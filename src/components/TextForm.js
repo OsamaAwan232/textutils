38c7793826320c6929e00c5138d4ac93813b2699
Osama Awan <osamaawan768@gmail.com>
2024-04-04 15:51:11 +0500

    Improving Dark Mode & Refactoring App Component

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -18,7 +18,9 @@ const TextForm = (props) => {
     }
 
     const handleCopyClick = ()=>{
-         navigator.clipboard.writeText(text);
+        let newtext = document.getElementById('Box')
+         newtext.select()
+         navigator.clipboard.writeText(newtext.value);
         }
     
     const handleOnchange = (event)=>{
@@ -27,22 +29,22 @@ const TextForm = (props) => {
     const [text, setText] = useState('')
     return (
         <>
-        <div className='container'>
+        <div className='container' style={{color: props.mode==="dark" ? 'white' : 'black' }}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-                <textarea className="form-control" value={text} onChange={handleOnchange} id="Box" rows="8"></textarea>
+                <textarea className="form-control" value={text} onChange={handleOnchange} style={{backgroundColor: props.mode==="dark" ? 'grey' : 'white' , color: props.mode==="dark" ? 'white' : 'black'  }} id="Box" rows="8"></textarea>
             </div>
             <button className="btn btn-danger mx-2" onClick={handleUpClick}>ToUpperCase</button>
             <button className="btn btn-danger mx-2" onClick={handleLoClick}>ToLowerCase</button>
             <button className="btn btn-danger mx-2" onClick={handleClearClick}>Clear Text</button>
             <button className="btn btn-danger mx-2" onClick={handleCopyClick}>Copy Text</button>
         </div>
-        <div className="container">
+        <div className="container" style={{color: props.mode==="dark" ? 'white' : 'black' }}>
             <h1>Your text summery</h1>
             <p>{text.split(' ').length} words and {text.length} characters</p>
             <p>{0.008 * text.slice(' ').length}Minutes read</p>
             <h2>Preview</h2>
-            <p>{text}</p>
+            <p>{text.length>0?text:"Enter Something in the text box to Preview it here"}</p>
         </div>
         </>
     
