897d565bab79e270da5d4dde6c029f789ade9e61
Osama Awan <osamaawan768@gmail.com>
2024-04-05 14:16:08 +0500

    React Router Setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
 import { useState } from 'react';
 import './App.css';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light')
@@ -34,12 +40,16 @@ function App() {
   }
   return (
     <>
-      <Navbar title='TextUtils' mode={mode} toggleMode={toggleMode}/>
-      <Alert alert= {alert}/>
-      <div className="container my-3">
-        <TextForm heading="Enter the text to analyze" showAlert={showAlert} mode={mode} />
-        {/* <About/> */}
-      </div>
+      <Router>
+        <Navbar title='TextUtils' mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/" element={<TextForm heading="Enter the text to analyze" showAlert={showAlert} mode={mode} />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
